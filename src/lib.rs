e7d5e1d83d04f81f8cef848fd82137074114cfe3
berrymot <merrybot47@gmail.com>
2024-02-09 14:34:01 -0800

    unprint

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,11 @@ pub fn fetch(user: &str, repo: &str, paths: Vec<&str>) -> Result<Files, Box<dyn 
     for path in paths {
         let url = format!("https://api.github.com/repos/{user}/{repo}/contents/{path}");
         let json = client.get(url).send()?.json::<serde_json::Value>()?;
-        println!("{}", json);
         let json = json.as_array().unwrap();
         for file in json {
             if Some("file") == file["type"].as_str() {
                 if let Some(name) = file["name"].as_str() {
-                    println!(": {name}");
+                    // println!(": {name}");
                     let file_url = file["download_url"].as_str().unwrap();
                     let content = client.get(file_url).send()?.bytes()?;
                     out.0.push(File {
@@ -29,14 +28,14 @@ pub fn fetch(user: &str, repo: &str, paths: Vec<&str>) -> Result<Files, Box<dyn 
                 }
             } else if Some("dir") == file["type"].as_str() {
                 if let Some(name) = file["name"].as_str() {
-                    println!("> {name}");
+                    // println!("> {name}");
                     for x in fetch(user, repo, vec![&format!("{path}/{name}")])
                         .unwrap()
                         .0
                     {
                         out.0.push(x);
                     }
-                    println!("< (up)");
+                    // println!("< (up)");
                 }
             }
         }
