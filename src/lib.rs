0b4f68143d3b7c1d286c6786eeae6686acd1075c
berrymot <merrybot47@gmail.com>
2024-02-11 00:41:23 -0800

    auth

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
-use reqwest::blocking::Client;
+use reqwest::{blocking::Client, header::AUTHORIZATION};
 use std::{error::Error, fs, path::Path};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct GhfcFile {
     pub name: String,
     pub content: Vec<u8>,
 }
+#[derive(Clone)]
 pub struct Files(pub Vec<GhfcFile>);
 
 pub fn fetch(
@@ -13,18 +14,23 @@ pub fn fetch(
     repo: &str,
     paths: Vec<&str>,
     recurse: bool,
+    token: &str,
 ) -> Result<Files, Box<dyn Error>> {
     let client = Client::builder().user_agent("gh-file-curler").build()?;
     let mut out = Files(vec![]);
     for path in paths {
         let url = format!("https://api.github.com/repos/{user}/{repo}/contents/{path}");
-        let json = client.get(url).send()?.json::<serde_json::Value>()?;
+        let json = client
+            .get(url)
+            .header(AUTHORIZATION, format!("Bearer {token}"))
+            .send()?
+            .json::<serde_json::Value>()?;
         let json = json.as_array().unwrap();
         for file in json {
             if Some("file") == file["type"].as_str() {
                 if let Some(name) = file["name"].as_str() {
                     if let Some(file_url) = file["download_url"].as_str() {
-                        println!(": {name}");
+                        // println!("{path}/{name}");
                         let content = client.get(file_url).send()?.bytes()?;
                         out.0.push(GhfcFile {
                             name: format!("{path}/{name}"),
@@ -35,14 +41,12 @@ pub fn fetch(
             } else if Some("dir") == file["type"].as_str() {
                 if recurse {
                     if let Some(name) = file["name"].as_str() {
-                        println!("> {name}");
-                        for x in fetch(user, repo, vec![&format!("{path}/{name}")], true)
+                        for x in fetch(user, repo, vec![&format!("{path}/{name}")], true, token)
                             .unwrap()
                             .0
                         {
                             out.0.push(x);
                         }
-                        println!("< (up)");
                     }
                 }
             }
